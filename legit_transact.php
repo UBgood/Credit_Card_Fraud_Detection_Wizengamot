7c3ac9b9cb37d06b6fad39140a419ecabdc129bb
susmit97 <64438682+susmit97@users.noreply.github.com>
2020-05-17 01:23:55 +0530

    Add files via upload

---
+++ legit_transact.php
@@ -0,0 +1,82 @@
+<?php
+$hostname="localhost"; //local server name default localhost
+
+$username="root";  //mysql username default is root.
+
+$password="";       //blank if no password is set for mysql.
+
+$database="student1";  //database name which you created
+
+$con=mysql_connect($hostname,$username,$password);
+
+if(! $con)
+{
+die('Connection Failed'.mysql_error());
+}
+
+mysql_select_db($database,$con);
+If(isset($_REQUEST['submit'])!='')
+{
+If($_REQUEST['name']=='')
+{
+Echo "please fill the empty field.";
+}
+Else
+{
+$sql="insert into student1(name) values('".$_REQUEST['name']."')";
+$res=mysql_query($sql);
+If($res)
+{
+//Echo "Fradaulent";
+}
+
+}
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head> <meta http-equiv="refresh" content="5">
+    <style>
+.myDiv {
+    
+    border: 2px solid black;
+  outline: cornsilk solid 10px;
+  margin: auto;  
+  padding: 20px;
+  text-align: center;
+    
+}
+        
+        .mDiv {
+    
+    padding-top: 240px;
+  
+        </style>
+    </head>
+<body>
+<div class="mDiv"></div>
+
+<div class="myDiv">
+<form action="trans.php">
+ 
+  <input type="text" id="name" name="name" value="Fraudulent Transaction" placeholder="Legitimate Transaction"><br>
+<script language="JavaScript">document.myform.submit();</script>
+ 
+</form>
+    
+ </div>
+</body>
+</html>
+
+<?php 
+function function_alert($message) { 
+      echo "<script>alert('$message');</script>"; 
+} 
+function_alert("Legitimate Transaction notified to Bank server");
+?>
+
+
+
---
+++ student1.sql
@@ -0,0 +1,37 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 16, 2020 at 08:49 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `student1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student1`
+--
+
+CREATE TABLE `student1` (
+  `name` varchar(36) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
